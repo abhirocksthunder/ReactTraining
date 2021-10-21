0358b23ae7800427c246d67ece7b02dea1125adb
abhilash <abhilashs@pllcsoftwaresolutions.com>
2021-10-21 16:57:44 +0530

    with weather

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+         private static readonly string[] Cities = new[]
+        {
+            "Brasília", "Sydney", "Vijayawada", "Peshawar", "Fukuoka", "Melbourne", "Basra", "Chengdu", "Suwon", "Tijuana"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            {
+                Id = Guid.NewGuid(),
+                Date = DateTime.Now.AddDays(1),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                City = Cities[index-1]
+            })
+            .ToArray();
+        }
+    }
+}
